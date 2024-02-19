ecf3a082b075074c530b0bfa663baf228eba082d
Xavitheforce <javier.miguelez.yague@gmail.com>
2024-02-19 12:11:52 +0100

    eliminar columnas

--- limpieza.py
+++ limpieza.py
@@ -6,7 +6,12 @@ df2019 = pandas.read_csv('data/champions-league-2019-WEuropeStandardTime.csv')
 df2020 = pandas.read_csv('data/champions-league-2020-UTC.csv')
 df2021 = pandas.read_csv('data/champions-league-2021-UTC.csv')
 df2022 = pandas.read_csv('data/champions-league-2022-UTC.csv')
-wins = pandas.read_csv('data/wins.csv')
 
-#saca la suma de todos los valores de la columna 'Champions'
-print(wins['Champions'].sum())
+# Eliminar las columnas que no interesan de los DataFrames (Date, Location, Group)
+
+df2017 = df2017.drop(["Date", "Location", "Group"], axis=1)
+df2018 = df2018.drop(["Date", "Location", "Group"], axis=1)
+df2019 = df2019.drop(["Date", "Location", "Group"], axis=1)
+df2020 = df2020.drop(["Date", "Location", "Group"], axis=1)
+df2021 = df2021.drop(["Date", "Location", "Group"], axis=1)
+df2022 = df2022.drop(["Date", "Location", "Group"], axis=1)
