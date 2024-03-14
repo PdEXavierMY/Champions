7a4856e7ec36dbc7daf2c7aa07a4f175e83574d6
Xavitheforce <javier.miguelez.yague@gmail.com>
2024-03-14 12:06:30 +0100

    intento de modificacion

---
+++ run.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+import os
+
+def create_virtual_environment():
+    # Comprobamos si el directorio 'venv' ya existe
+    if not os.path.exists('venv'):
+        print("Creando entorno virtual...")
+        # Creamos el entorno virtual usando subprocess
+        subprocess.check_call([sys.executable, '-m', 'venv', 'venv'])
+        print("Entorno virtual creado exitosamente.")
+    else:
+        print("El entorno virtual ya existe.")
+
+def install_dependencies():
+    print("Instalando dependencias...")
+    # Instalamos las dependencias utilizando pip
+    subprocess.check_call([os.path.join('venv', 'bin', 'pip'), 'install', '-r', 'requirements.txt'])
+    print("Dependencias instaladas exitosamente.")
+
+def main():
+    create_virtual_environment()
+    # Activamos el entorno virtual antes de instalar las dependencias
+    activate_script = os.path.join('venv', 'Scripts', 'activate')
+    exec(open(activate_script).read(), {'__file__': activate_script})
+    install_dependencies()
+
+if __name__ == "__main__":
+    main()
